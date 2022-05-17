c4da8c061f1ff8aa207b1156a8039328eca9f7c7
Mia / Søren <sowel20@student.sdu.dk>
2022-05-17 03:13:09 +0200

    Update lfs.c

--- lfs.c
+++ lfs.c
@@ -56,7 +56,7 @@ int lfs_getattr( const char *path, struct stat *stbuf ) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 	} else {
-		printf("Reaches else");
+		printf("Reaches else\nCurrent Folder Index: %d\nCurrent File Index: %d\n"); // It reaches here after mkdir. Why?
 		res = -ENOENT;
 	}
 	return res;
